6500fbd6ebf1ff4625d945fe7c7f7a555eafe9fb
Shreyas Bhat <sbhat@fnal.gov>
2024-03-18 14:42:10 -0500

    Added subtraction function to awesome

--- test_awesome.py
+++ test_awesome.py
@@ -1,6 +1,6 @@
 import pytest
 
-from awesome import add_two_ints
+from awesome import add_two_ints, subtract_two_ints
 
 
 @pytest.mark.parametrize(["int1", "int2", "expected"],
@@ -30,3 +30,8 @@ def test_add_two_ints_good(int1, int2, expected):
 def test_add_two_ints_bad_type(int1, int2):
     with pytest.raises(TypeError, match="One of the input values is NOT an int!!"):
         add_two_ints(int1, int2)
+
+
+@pytest.mark.unit
+def test_subtract_two_ints():
+    assert subtract_two_ints(6,3) == 3
