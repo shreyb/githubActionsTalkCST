e3e1f0817d8fd0d5ab8bc4acc61b4aaac1ff27b8
Shreyas Bhat <sbhat@fnal.gov>
2024-03-18 15:00:23 -0500

    Revert "Added subtraction function to awesome"
    This reverts commit 6500fbd6ebf1ff4625d945fe7c7f7a555eafe9fb.

--- awesome.py
+++ awesome.py
@@ -4,8 +4,3 @@ def add_two_ints(a: int, b: int) -> int:
     if (not isinstance(a, int)) or (not isinstance(b,int)):
         raise TypeError("One of the input values is NOT an int!!")
     return a + b
-
-def subtract_two_ints(a: int, b: int) -> int:
-    if (not isinstance(a, int)) or (not isinstance(b,int)):
-        raise TypeError("One of the input values is NOT an int!!")
-    return a + b
