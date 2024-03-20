436de41c86f0b85d2974015a4cb3a0e60ec35486
Shreyas Bhat <sbhat@fnal.gov>
2024-03-20 14:48:28 -0500

    Added a subtraction function

--- awesome.py
+++ awesome.py
@@ -4,3 +4,8 @@ def add_two_ints(a: int, b: int) -> int:
     if (not isinstance(a, int)) or (not isinstance(b,int)):
         raise TypeError("One of the input values is NOT an int!!")
     return a + b
+
+def subtract_two_ints(a: int, b: int) -> int:
+    if (not isinstance(a, int)) or (not isinstance(b,int)):
+        raise TypeError("One of the input values is NOT an int!!")
+    return a + b
